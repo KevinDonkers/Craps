57b26c05f10715b4a044d70ef0fc6d4b1db0846b
justinsearle <justin.searle@sympatico.ca>
2016-08-15 20:30:21 -0400

    commenetd and cleaned some code

--- Craps/Main.cpp
+++ Craps/Main.cpp
@@ -10,29 +10,30 @@ using namespace std;
 //main function to execute all the code
 int main(){
 	string userName, additionalBets;
-	int passChoice, point, betIndex, subPassChoice, subPassValue;
+	int passChoice, point, betIndex, subPassChoice, subPassValue, total;
 	bool roundEnd = false;
 
 	Roll roll;
 	vector<Bet> bets;
 
+	//ask for username
 	cout << "What is your Username?" << endl;
 	while (!(cin >> userName)) {
 		cout << "Bad Input" << endl;
 	}
 
 	//check the username in the user file
-	User user = checkAccount(userName);
+	User user = checkAccount(userName);	
 
-	cout << user.username << ", " << user.fullname << ", " << user.money << endl;
-
-	cout << "Hello " << user.fullname << ", Welcome to Craps" << endl;
+	//welcome message
+	cout << endl << "Hello " << user.fullname << ", Welcome to Craps." << endl;
 	cout << "What would you like to bet?" << endl;
 
 	//game loop
 	while (true) {
 
 		//menu
+		cout << endl << "You have: $" << user.money << " to bet." << endl;
 		cout << "1. Pass Line" << endl;
 		cout << "2. Don't Pass Line" << endl;
 		cout << "3. Exit" << endl;
@@ -42,35 +43,61 @@ int main(){
 			cout << "Bad Input" << endl;
 		}
 
-		//exit
+		//exit game loop
 		if (passChoice == 3) {
 			break;
 		}
 
-		//more bets?
-		cout << "Do you want additional bets? (y/n)";
+		//roll the dice
+		roll = RollDice();
+		Bet choice;
 
+		//roll checks
+		if (passChoice == 1) {//bet on pass line			
+			choice.name = "pass line";						
+		}
+		else if (passChoice == 2) {//bet on dont pass line			
+			choice.name = "dont pass line";
+		}
+
+		//ask for bet amount
+		cout << "How much would you like to bet?" << endl;
+		while (!(cin >> choice.value) || choice.value < 1 || choice.value > user.money) {
+			cout << "Bad Input" << endl;
+		}
+
+		//add bet to the vector of bets
+		bets.push_back(choice);
+
+		//more bets?
+		cout << "Do you want additional bets? (y/n): ";
 		while (!(cin >> additionalBets)) {
 			cout << "Bad Input" << endl;
 
 		}
 
-		while (additionalBets == "y" || additionalBets == "Y") {
-
+		//only print menu once for additional bets
+		if ((additionalBets == "y" || additionalBets == "Y")) {
 			cout << "1. Aces or box cars" << endl;
 			cout << "2. Eleven" << endl;
 			cout << "3. Ace Duece" << endl;
 			cout << "4. Done Betting." << endl;
+		}
+		
+		//loop for additional bet menu
+		while (additionalBets == "y" || additionalBets == "Y") {
 
 			//get valid input
 			while (!(cin >> subPassChoice) || subPassChoice < 1 || subPassChoice > 4) {
 				cout << "Bad Input" << endl;
 			}
 
+			//break from additional bet loop
 			if (subPassChoice == 4) {
 				break;
 			}
 
+			//get bet amount
 			cout << "How much would you like to bet?" << endl;
 			Bet additionalChoice;
 
@@ -81,6 +108,7 @@ int main(){
 
 			additionalChoice.value = subPassValue;
 
+			//store the bet name based on choice
 			switch (subPassChoice) {
 
 				case 1:
@@ -96,135 +124,79 @@ int main(){
 					break;
 			}
 			bets.push_back(additionalChoice);
-		}
 
-		roll = RollDice();
+			cout << "Type another bet, or 4 to exit" << endl;
+		} //end of additional bet loop
+
+		//show roll
+		cout << "\nYou rolled " << roll.total << " (" << roll.die1 << " & " << roll.die2 << ")\n";
+
+		//roll check
+		if (passChoice == 1 && (betIndex = findBet("pass line", bets)) >= 0) { //bet on pass line
+
+			if ((roll.total == 7 || roll.total == 11)) {
+				bets.at(betIndex).won = true;
+				roundEnd = true;
+			}
+			else if (roll.total == 2 || roll.total == 3 || roll.total == 12) {
+				bets.at(betIndex).won = false;
+				roundEnd = true;
+			}			
+
+		} else if (passChoice == 2  && (betIndex = findBet("dont pass line", bets)) >= 0) {//bet on dont pass line
+
+			if (roll.total == 7 || roll.total == 11) {
+				bets.at(betIndex).won = false;
+				roundEnd = true;
+			}
+			else if (roll.total == 2 || roll.total == 3 || roll.total == 12) {
+				bets.at(betIndex).won = true;
+				roundEnd = true;
+			}			
+
+		}				
 
 		//check additional bets
-		if (roll.total == 11) {
-			if ((betIndex = findBet("eleven", bets)) >= 0) {
-				bets.at(betIndex).won = true;
-			}
+		if (roll.total == 11 && (betIndex = findBet("eleven", bets)) >= 0) {
+			bets.at(betIndex).won = true;			
 		}
-		else {
-			if ((betIndex = findBet("eleven", bets)) >= 0) {
-				bets.at(betIndex).won = false;
-			}
+		else if (roll.total == 12 && (betIndex = findBet("aces or box cars", bets)) >= 0) {
+			bets.at(betIndex).won = true;			
 		}
-
-		if (roll.total == 12) {
-			if ((betIndex = findBet("aces or box cars", bets)) >= 0) {
-				bets.at(betIndex).won = true;
-			}
-		}
-		else {
-			if ((betIndex = findBet("aces or box cars", bets)) >= 0) {
-				bets.at(betIndex).won = false;
-			}
-		}
-
-		if (roll.total == 3) {
-			if ((betIndex = findBet("ace duece", bets)) >= 0) {
-				bets.at(betIndex).won = true;
-			}
-		}
-		else {
-			if ((betIndex = findBet("ace duece", bets)) >= 0) {
-				bets.at(betIndex).won = false;
-			}
-		}
-
-		//come out roll checks
-		if (passChoice == 1) {//bet on pass line
-			Bet choice1;
-			choice1.name = "Pass Line";
-
-			// Fetch tab key state.
-			//SHORT tabKeyState = GetAsyncKeyState(VK_TAB);
-			//bool tab = false;
-			// Test high bit - if set, key was down when GetAsyncKeyState was called.
-			//if ((1 << 16) & tabKeyState)
-			//{
-			//	tab = true;
-			//}
-
-			cout << "How much would you like to bet on the pass line?" << endl;
-			while (!(cin >> choice1.value) || choice1.value < 1 || choice1.value > user.money) {
-				cout << "Bad Input" << endl;
-			}
-
-			cout << "\nYou rolled " << roll.total << " (" << roll.die1 << " & " << roll.die2 << ")\n";
-
-			if (roll.total == 7 || roll.total == 11) {
-				cout << "You Win!" << endl;
-				roundEnd = true;
-				choice1.won = true;
-			}
-			else if (roll.total == 2 || roll.total == 3 || roll.total == 12) {
-				cout << "You Lose!" << endl;
-				roundEnd = true;
-				choice1.won = false;
-			}
-
-			//if (tab) {
-			//	choice1.value = user.money;
-			//}
-			bets.push_back(choice1);
-		}
-		else if (passChoice == 2) {//bet on dont pass line
-			Bet choice2;
-			choice2.name = "Don't Pass Line";
-
-			cout << "How much would you like to bet on the don't pass line?" << endl;
-			while (!(cin >> choice2.value) || choice2.value < 1 || choice2.value > user.money) {
-				cout << "Bad Input" << endl;
-			}
-
-			if (roll.total == 7 || roll.total == 11) {
-				cout << "You Lose!" << endl;
-				roundEnd = true;
-				choice2.won = false;
-			}
-			else if (roll.total == 2 || roll.total == 3 || roll.total == 12) {
-				cout << "You Win!" << endl;
-				roundEnd = true;
-				choice2.won = true;
-			}
-			bets.push_back(choice2);
+		else if (roll.total == 3 && (betIndex = findBet("ace duece", bets)) >= 0) {
+			bets.at(betIndex).won = true;			
 		}
 
 		/*
-		 *
+		 * second round
 		 */
 		if (!roundEnd) {//into point rolls
 
-			//for (int i = 0; i < bets.size(); i++) {
-			//	bets.at(i).won = false;
-			//}
-		
 			point = roll.total;
 
-			//my comment
-			while (true) {
+			//menu and stuff
+			cout << "4. Bet on 4 being rolled." << endl;
+			cout << "5. Bet on 5 being rolled." << endl;
+			cout << "6. Bet on 6 being rolled." << endl;
+			cout << "8. Bet on 8 being rolled." << endl;
+			cout << "9. Bet on 9 being rolled." << endl;
+			cout << "10. Bet on 10 being rolled." << endl;
+			cout << "7. Exit." << endl;
 
-				//menu and stuff
-				cout << "4. Bet on 4 being rolled." << endl;
-				cout << "5. Bet on 5 being rolled." << endl;
-				cout << "6. Bet on 6 being rolled." << endl;
-				cout << "8. Bet on 8 being rolled." << endl;
-				cout << "9. Bet on 9 being rolled." << endl;
-				cout << "10. Bet on 10 being rolled." << endl;
-				cout << "7. Exit." << endl;
+			//more bets loop
+			while (true) {
 
 				//get valid input
 				while (!(cin >> subPassChoice) || subPassChoice < 4 || subPassChoice > 10) {
 					cout << "Bad Input" << endl;
 				}
 
+				//break from more bets loop
 				if (subPassChoice == 7) {
 					break;
 				}
 
+				//get bet amount
 				cout << "How much would you like to bet?" << endl;
 				Bet additionalChoice;
 
@@ -233,13 +205,16 @@ int main(){
 					cout << "Bad Input" << endl;
 				}
 
+				//
 				additionalChoice.value = subPassValue;
 				additionalChoice.name = "bet " + to_string(subPassChoice);
 				
+				//
 				bets.push_back(additionalChoice);
+				cout << "Type another bet, or 7 to exit" << endl;
 			}
 
-
+			//show roll
 			cout << "You rolled a point of " << point << endl;
 			do {
 				cout.flush();
@@ -250,46 +225,23 @@ int main(){
 				cout << "\nYou rolled " << roll.total << " (" << roll.die1 << " & " << roll.die2 << ")\n";
 
 				//check bets
-				if (roll.total == 4) {		
-					
-					if ((betIndex = findBet("bet 4", bets)) >= 0) {
-						cout << " TEST 4\n\n";
-						cout << betIndex;
-						bets.at(betIndex).won = true;
-					}
+				if (roll.total == 4 && (betIndex = findBet("bet 4", bets)) >= 0) {						
+					bets.at(betIndex).won = true;					
 				}
-				else if (roll.total == 5) {
-					if ((betIndex = findBet("bet 5", bets)) >= 0) {					
-						cout << " TEST 5\n\n";
-						cout << betIndex;
-						bets.at(betIndex).won = true;
-					}
+				else if (roll.total == 5 && (betIndex = findBet("bet 5", bets)) >= 0) {					
+					bets.at(betIndex).won = true;					
 				}
-				else if (roll.total == 6) {					
-					if ((betIndex = findBet("bet 6", bets)) >= 0) {
-						cout << " TEST 6\n\n";
-						cout << betIndex;
-						bets.at(betIndex).won = true;
-					}
+				else if (roll.total == 6 && (betIndex = findBet("bet 6", bets)) >= 0) {
+					bets.at(betIndex).won = true;					
 				}
-				else if (roll.total == 8) {					
-					if ((betIndex = findBet("bet 8", bets)) >= 0) {
-						cout << " TEST 8\n\n";
-						cout << betIndex;
-						bets.at(betIndex).won = true;
-					}
+				else if (roll.total == 8 && (betIndex = findBet("bet 8", bets)) >= 0) {
+					bets.at(betIndex).won = true;					
 				}
-				else if (roll.total == 9) {					
-					if ((betIndex = findBet("bet 9", bets)) >= 0) {
-						cout << " TEST 9\n\n";
-						cout << betIndex;
-						bets.at(betIndex).won = true;
-					}
+				else if (roll.total == 9 && (betIndex = findBet("bet 9", bets)) >= 0) {
+					bets.at(betIndex).won = true;					
 				}
-				else if (roll.total == 10) {
-					if ((betIndex = findBet("bet 10", bets)) >= 0) {
-						bets.at(betIndex).won = true;
-					}
+				else if (roll.total == 10 && (betIndex = findBet("bet 10", bets)) >= 0) {
+					bets.at(betIndex).won = true;				
 				}
 				
 			} while (roll.total != point && roll.total != 7);
@@ -317,8 +269,8 @@ int main(){
 					}
 				}
 				else {
-					cout << "You rolled a 7 before the point again, You Win!" << endl;
 					if ((betIndex = findBet("Don't Pass Line", bets)) >= 0) {
+					cout << "You rolled a 7 before the point again, You Win!" << endl;
 						bets.at(betIndex).won = true;
 					}
 				}
@@ -329,19 +281,21 @@ int main(){
 			roundEnd = false;
 		}
 
-		cout << "\nwinnings: " << calculateWinnings(bets) << endl;
-		cout << "user money: " << user.money << endl;
+		//calculate winnings and display to user		
+		user.money += total = calculateWinnings(bets);
 
-		user.money += calculateWinnings(bets);
-
+		//loop through and show user what they won/lost
 		for (int i = 0; i < bets.size(); i++) {
-			cout << endl << bets.at(i).name << ", " << bets.at(i).value << ", " << bets.at(i).won << endl;
+
+			cout << (bets.at(i).won ? "You won " : "You lost ");
+			cout << bets.at(i).name << " for " << bets.at(i).value << endl;// << ", " << bets.at(i).won << endl;
 		}
+		cout << endl << "Total winnings: " << total << endl;
 
-		cout << "after user money: " << user.money << endl;
-
+		//save to file
 		updateUserList(user);
 
+		//empty the bet vector
 		bets.clear();
 
 	} //end of loop
